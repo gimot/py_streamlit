ce8fd6eaecc09e0bcc084bee42e2e6c6128a047a
藤田基紘 <fujitamotohiro@abc.local>
2022-06-13 09:59:28 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import time
+
+st.title('Streamlit 超入門')
+
+st.write('プレグレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!'
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander = st.expander('問い合わせ1')
+expander.write('問い合わせ1の回答')
+expander = st.expander('問い合わせ2')
+expander.write('問い合わせ2の回答')
+expander = st.expander('問い合わせ3')
+expander.write('問い合わせ3の回答')
+
+# text = st.text_input('あなたの趣味を教えてください')
+# condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+#
+# 'あなたの趣味：', text
+# 'コンディション:', condition
+
+
+# if st.checkbox('Show Image'):
+#     img = Image.open('cat.jpeg')
+#     st.image(img, caption='CAT', use_column_width=True)
+
+
+#df = pd.DataFrame(
+#    np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
+#    columns=['lat', 'lon']
+#)
+#st.map(df)
